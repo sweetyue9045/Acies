d3894191c32f9f023873e1c72f495d0c1d90e77a
sweetyue9045 <sweetyue9045@gmail.com>
2021-11-01 14:43:51 +0800

    feature css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import HOME from './pages/Home'
-import { BrowserRouter, Route, Switch} from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 function App() {
   return (
     <BrowserRouter>
-    <Switch>
-          <Route exact path="/" component={HOME} />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={HOME} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/components/News.js
+++ src/components/News.js
@@ -8,7 +8,16 @@ export default function News() {
     return (
         <div>
             <div className="feature">
-             <img src={LOGO} />
+                <img src={LOGO} />
+                <div className="content">
+                    眼動儀與手把結合
+                </div>
+                <div className="content">
+                    2D橫向卷軸動作解謎遊戲
+                </div>
+                <div className="content">
+                    突如其然的一場意外造成妹妹薇妲失去身體，為了尋回妹妹的身體，玩家將扮演一對雙胞胎姊妹，前往三個神秘部落，展開一場冒險旅程。
+                </div>
             </div>
         </div>
     );
