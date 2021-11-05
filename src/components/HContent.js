711e65d73c5fd521eb764d5d448cfc441d6a212c
Hung1029 <psi94133@gmail.com>
2021-11-06 01:21:20 +0800

    sister_half

--- src/components/HContent.js
+++ src/components/HContent.js
@@ -14,31 +14,79 @@ import SISTER_R from "../assets/im/sister_R.png";
 import SISTER_R2 from "../assets/im/sister_R2.png";
 import SISTER_L2 from "../assets/im/sister_L2.png";
 
+import BG_TEXT from "../assets/im/bg_text.png";
+import BG_TEXT2 from "../assets/im/bg_text2.png";
+
+import BTN_CANCEL from "../assets/im/cancel.svg";
+
 export default function HContent() {
-    var click =function(element) {
-       // console.log(document.getElementById(element.target.id).src);
-       // var srcc=document.getElementById(element.target.id).id;       
-       if (element.target.id === "SISTER_R"){
-       // document.getElementById(element.target.id).src=SISTER_R2;
-       document.getElementById(element.target.id).classList.add("flip");
-       var resetAnimation = function() {
-           document.getElementById(element.target.id).classList.add("flip-reverse");
-           document.getElementById(element.target.id).classList.remove("flip");
-           document.getElementById(element.target.id).src=SISTER_R2;
-         };
-       setTimeout(resetAnimation, 1000);
-       }
-       else if(element.target.id === "SISTER_L"){
-        document.getElementById(element.target.id).classList.add("flip");
-        var resetAnimation = function() {
-            document.getElementById(element.target.id).classList.add("flip-reverse");
-            document.getElementById(element.target.id).classList.remove("flip");
-            document.getElementById(element.target.id).src=SISTER_L2;
-          };
-        setTimeout(resetAnimation, 1000);
+
+    var click = function (element) {
+        if (element.target.id === "sister_R") {
+            document.getElementById(element.target.id).classList.remove("state");
+            document.getElementById(element.target.id).classList.add("flip");
+            document.getElementById('sister_L').classList.add("leftfadeout");
+            document.getElementById('textL').classList.add("leftfadein");
+
+            document.getElementById('page2L').classList.add("leftfadein");
+            var flipAnimation = function () {
+                document.getElementById(element.target.id).classList.add("flip-reverse");
+                document.getElementById(element.target.id).classList.remove("flip");
+                document.getElementById(element.target.id).src = SISTER_R2;
+                document.getElementById('nameL').classList.add("displaynone");
+                document.getElementById('sister_L').classList.add("displaynone");
+               
+                //document.getElementById('nameL').style.display = "none";
+                document.getElementById('page2L').style.display = "block";
+                document.getElementById('textL').style.display = "block";
+            };
+            setTimeout(flipAnimation, 800);
+        }
+        else if (element.target.id === "sister_L") {
+            document.getElementById(element.target.id).classList.remove("state");
+            document.getElementById(element.target.id).classList.add("flip");
+            document.getElementById('sister_R').classList.add("rightfadeout");
+            document.getElementById('textR').classList.add("rightfadein");
+
+            document.getElementById('page2R').classList.add("rightfadein");
+            var flipAnimation = function () {
+                document.getElementById(element.target.id).classList.add("flip-reverse");
+                document.getElementById(element.target.id).classList.remove("flip");
+                document.getElementById(element.target.id).src = SISTER_L2;
+                document.getElementById('sister_R').style.display = "none";
+                document.getElementById('nameR').style.display = "none";
+                document.getElementById('page2R').style.display = "block";
+                document.getElementById('textR').style.display = "block";
+            };
+            setTimeout(flipAnimation, 800);
+        }
+
+    }
+    
+    var animL = function () {
+        document.getElementById('nameL').classList.remove("displaynone");
+
+       // document.getElementById('sister_R').classList.remove("flip-reverse");
+        document.getElementById('sister_R').classList.add("flip");
+       // document.getElementById('textL').classList.remove("leftfadein");
+        document.getElementById('textL').classList.add("leftfadeout");
+        document.getElementById('page2L').classList.add("leftfadeout");
+       // document.getElementById('page2L').classList.remove("leftfadein");
+        document.getElementById('sister_L').classList.add("flip-reverse");
+        document.getElementById('sister_L').src=SISTER_L;
         
-       }
-       
+
+        var flipAnimation = function () {
+            document.getElementById('sister_R').classList.add("flip-reverse");
+            document.getElementById('sister_R').src=SISTER_R;
+           document.getElementById('textL').style.display="none";
+           document.getElementById('page2L').style.display="none";
+           document.getElementById('sister_L').style.display="block";
+        };
+        setTimeout(flipAnimation, 800);
+    }
+    var animR = function () {
+
     }
     return (
         <div>
@@ -53,23 +101,31 @@ export default function HContent() {
             </div>
             <div className="sister">
                 <div className="page">
-                    <div className="name">
+                    <div className="name" id="nameL">
                         <img src={ARROW_L} />
                         <div className="title_text">薇妲</div>
                         <img src={ARROW_R} />
                     </div>
                     <div className="sisimg">
-                        <img id="SISTER_L"  onClick={click.bind(this)} src={SISTER_L} />
+                        <img className="state" id="sister_L" onClick={click.bind(this)} src={SISTER_L} />
+                        <img id="page2L" className="page2" src={BG_TEXT} />
+
+                        <div id="textL" className="text"> <img id="cancelL" onClick={animL} src={BTN_CANCEL} />雙胞胎中的姊姊<br />個性驕傲嚴謹，內心是個溫柔的人。因為總會管教妹妹，兩人之間發生不少爭執。<br /><br />冒險旅途中會披上祖傳披風，也會增加許多民俗感的小元素。</div>
                     </div>
                 </div>
+
                 <div className="page">
-                    <div className="name">
+
+                    <div className="name" id="nameR">
                         <img src={ARROW_L} />
                         <div className="title_text">莉妲</div>
                         <img src={ARROW_R} />
                     </div>
-                    <div className="sisimg">
-                        <img id="SISTER_R" src={SISTER_R} onClick={click.bind(this)} />
+                    <div className="sisimg" >
+                        <img id="sister_R" className="state" src={SISTER_R} onClick={click.bind(this)} />
+
+                        <img id="page2R" className="page2" src={BG_TEXT2} />
+                        <div id="textR" className="text"> <img id="cancelR" onClick={animR()} src={BTN_CANCEL} />雙胞胎中的妹妹<br />個性活潑開朗、 樂於助人，容易衝動 犯錯，內心渴望於他人的認可。<br /><br />冒險旅途中會變成靈魂型態，保有原本特色並且跟隨在姊姊身旁。</div>
                     </div>
                 </div>
             </div>
