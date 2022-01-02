66fc36fc55fe8e56d6598a9ccca052959ab453d0
wk9 <sweetyue9045@gmail.com>
2022-01-03 03:22:30 +0800

    arrow lottie

--- src/components/Home/H_Enter.js
+++ src/components/Home/H_Enter.js
@@ -3,21 +3,46 @@ import GIF from "../../assets/im/start_gif.gif";
 import PARALLAX from "../../assets/im/start_parallax.png";
 import LOGO from "../../assets/im/start_logo.svg";
 import LOGO_BG from "../../assets/im/start_logo_bg.png";
+import arrow from "../../assets/lotties/arrow.json";
 
+import Lottie from "react-lottie";
 import { useContext } from "react";
 import { ScrollY } from "../../pages/Home";
 
 export default function Enter() {
     const offsetY = useContext(ScrollY);
-    const style = {};
-    if (document.body.clientWidth <= 834) style.top = 0;
-    else style.top = offsetY * 0.3 + "px";
+    const gifstyle = {};
+    const arrowstyle = {}
+    if (document.body.clientWidth <= 834) {
+        gifstyle.top = 0;
+        arrowstyle.top = "80vh";
+    }
+    else {
+        gifstyle.top = offsetY * 0.3 + "px";
+        arrowstyle.top = "calc(80vh + "+offsetY * 0.3 + "px)";
+    }
 
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: arrow,
+
+        renderSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
     return (
         <>
             <img src={GIF} className="img_gif" alt="GIF"
-                style={style}
+                style={gifstyle}
             />
+            <div className="home_arrow" style={arrowstyle}>
+                <Lottie
+                    options={defaultOptions}
+                    height={100}
+                    width={100}
+                />
+            </div>
             <div className="enter_top" id="enter">
                 <div className="logo" >
                     <img src={LOGO_BG} alt="LOGO_BG" />
